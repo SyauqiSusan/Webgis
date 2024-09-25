0c3eb8c38e8ef2e7e60493d28d0eee4108430505
SyauqiSusan <167614738+SyauqiSusan@users.noreply.github.com>
2024-09-25 11:08:48 +0700

    Add files via upload

---
+++ data/Jalan_kab_4.js
@@ -0,0 +1,8 @@
+var json_Jalan_kab_4 = {
+"type": "FeatureCollection",
+"name": "Jalan_kab_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
